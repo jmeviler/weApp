1f1a2e42b54308afe5840fe8ce1d58398e06ea86
Ben Li <benli@augmentum.com.cn>
2016-11-21 20:37:18 +0800

    add bus 3

--- pages/bus/busDetail.js
+++ pages/bus/busDetail.js
@@ -3,6 +3,7 @@ Page({
     name: '',
     stopId: '',
     direction: 0,
+    tips: '',
     busInfo: {},
     stations: {},
     stationsLeft: {},
@@ -22,6 +23,7 @@ Page({
         var stations = res.data.lineResults0;
         stations.start = res.data.busLine.start_stop;
         stations.end = res.data.busLine.end_stop;
+        console.error(res.data.busLine);
         vm.setData({
           name: name,
           stations: stations,
@@ -35,8 +37,33 @@ Page({
 
   bindClickStop: function(e) {
       var vm = this;
-      vm.setData({
-          stopId: e.target.id
-      });
+      var name = vm.data.name;
+      var lineId = vm.data.busInfo.line_id;
+      var stopId = e.target.id;
+
+      wx.request({
+      url: 'https://robot.leanapp.cn/api/busstop/'+name+'/'+lineId+'/'+stopId+'/1',
+      header: {
+          'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        var tips = '';
+        if (res.data.cars.length) {
+          var terminal = res.data.cars[0].terminal;
+          var stopdis = res.data.cars[0].stopdis;
+          var time = res.data.cars[0].time;
+          console.error(typeof time)
+
+          if (time !== 'null') {
+            tips = terminal+'还有'+stopdis+'站，约'+Math.ceil(time / 60)+'分钟';
+          }
+        }
+
+        vm.setData({
+          stopId: stopId,
+          tips: tips
+        });
+      }
+    });
   }
 })
